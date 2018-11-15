95be38f53e045834afc47489b87b8dfe7da096d1
Tony <websieu@gmail.com>
2018-11-15 14:08:59 +0700

    update url

--- VakaIDServer/Program.cs
+++ VakaIDServer/Program.cs
@@ -42,7 +42,7 @@ namespace VakaxaIDServer
                 .CreateLogger();
 
             return WebHost.CreateDefaultBuilder(args)
-                    .UseUrls("http://0.0.0.0:5000")
+                    .UseUrls("http://127.0.0.1:5000")
                     .UseStartup<Startup>()
                     .ConfigureLogging(builder =>
                     {
