9d1005c2ba9f1c48588c863d75e2148fe665f85a
Ngô Ngọc Huấn <huannn@vakaxa.com>
2018-11-23 10:11:28 +0700

    disable form profile

--- VakaIDServer/Quickstart/Profile/PersonalDetails.cs
+++ VakaIDServer/Quickstart/Profile/PersonalDetails.cs
@@ -7,16 +7,18 @@ namespace VakaxaIDServer.Quickstart.Profile
     public class PersonalDetails
     {
         [Required]
+        [MaxLength(255)]
         public string StreetAddress { get; set; }
         [Required]
         [DataType(DataType.Date)]
         public DateTime Birthday { get; set; }
         [Required]
+        [MaxLength(255)]
         public string Occupation { get; set; }
         [Required]
+        [MaxLength(255)]
         public string About { get; set; }
         [Required]
-        
         public string Gender { get; set; }
     }
 }
--- VakaIDServer/Quickstart/Profile/SocialNetwork.cs
+++ VakaIDServer/Quickstart/Profile/SocialNetwork.cs
@@ -1,19 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace VakaxaIDServer.Quickstart.Profile
 {
     public class SocialNetwork
     {
+        [MaxLength(255)]
         public string Website { get; set; }
-
+        [MaxLength(255)]
         public string Facebook { get; set; }
-
+        [MaxLength(255)]
         public string Twitter { get; set; }
-
+        [MaxLength(255)]
         public string Telegram { get; set; }
-        
+        [MaxLength(255)]
         public string Skype { get; set; }
-
+        [MaxLength(255)]
         public string LinkedIn { get; set; }
 
         public static SocialNetwork FromJson(string json) =>
